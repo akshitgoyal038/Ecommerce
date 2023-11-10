c5f42427cb5c69c1e654fe953f512404b76b1ee7
akshitgoyal038 <akshitgoyal038@gmail.com>
2023-11-10 12:45:34 +0530

    Fronent started

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -7,7 +7,7 @@ const {auth,
 isAdmin,isUser} =  require("../middleware/auth");
 
 router.post("/order/new",auth,newOrder);
-router.get("/order/:id",auth,isAdmin,getSingleOrder);
+router.get("/order/:id",auth,getSingleOrder);
 router.get("/order/me",auth,myOrders);
 
 module.exports = router;
